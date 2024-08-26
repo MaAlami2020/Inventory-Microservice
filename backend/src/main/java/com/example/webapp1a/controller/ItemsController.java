0a5e46fd6508dd269ad2f86349329e713d406112
MaAlami2020 <ma.alami.2020@alumnos.urjc.es>
2024-08-26 13:56:35 +0200

    complete implementation of products searching

--- backend/src/main/java/com/example/webapp1a/controller/ItemsController.java
+++ backend/src/main/java/com/example/webapp1a/controller/ItemsController.java
@@ -1,29 +1,34 @@
 package com.example.webapp1a.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.webapp1a.Model.Item;
+import com.example.webapp1a.Service.ItemService;
 
 
 @Controller
 public class ItemsController {
 
+    @Autowired
+    private ItemService itemService;
+
     @GetMapping("/items")
     public String getItems(Model model, Pageable page){
-
-        //Page<Item> items = itemService.findAll(PageRequest.of(0,10));
-        //model.addAttribute("items", items);
         return "index";
     }
 
     @PostMapping("/items/name")
-    public String postMethodName(Model model, @RequestBody String name) {
-        model.addAttribute("itemToFound", name);
-        
+    public String postMethodName(Model model, Pageable page, @RequestParam String name) {
+        Page<Item> itemsFounded = itemService.getItemsByName(name, PageRequest.of(0,10));
+        model.addAttribute("items", itemsFounded);        
         return "index";
     }
     
